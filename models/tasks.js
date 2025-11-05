3904e5b1f2ae1999e753f3170231be600e2a588c
Yash shukla <newyashshukla15@gmail.com>
2025-11-06 00:50:36 +0530

    Create tasks.js

---
+++ models/tasks.js
@@ -0,0 +1,8 @@
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  deadline: { type: Date, required: true },
+  completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
