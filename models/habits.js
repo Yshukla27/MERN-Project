9965b38ef69aa50bdda76381188668d0eeaca678
Yash shukla <newyashshukla15@gmail.com>
2025-11-06 00:50:08 +0530

    Create habits.js

---
+++ models/habits.js
@@ -0,0 +1,18 @@
+onst mongoose = require('mongoose');
+
+const habitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  daysPerformed: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Habit', habitSchema);
